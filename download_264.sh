b03c56e45bb4d9b44d5a2188cce853b1ea439be6
Peter Li <qq1800@gmail.com>
2017-08-05 04:48:14 +0000

    install and use ffmpeg to extract frames

---
+++ download_264.sh
@@ -0,0 +1,2 @@
+wget --no-check-certificate "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21482&authkey=ALgHdrTsWIC36LY"
+mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21482&authkey=ALgHdrTsWIC36LY" 1.264
