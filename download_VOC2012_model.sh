91c316e007f63ae332699a84dd35b67c4f4afd0e
Peter Li <qq1800@gmail.com>
2017-08-16 20:56:30 +0000

    way to download re-trained model file

--- download_VOC2012_model.sh
+++ download_VOC2012_model.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 wget --no-check-certificate "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21483&authkey=ABx_ACRHH0g3WyE"
 mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21483&authkey=ABx_ACRHH0g3WyE" model_frcnn.hdf5
+wget --no-check-certificate "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21487&authkey=ACAQeaiFqEzF0PI"
+mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21487&authkey=ACAQeaiFqEzF0PI" retrain_model.hdf5
+
 
