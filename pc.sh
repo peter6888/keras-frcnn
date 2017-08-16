78a8791cfd8a0e4e4ac886e73e2f48b8d064a249
Peter Li <qq1800@gmail.com>
2017-08-16 18:50:56 +0000

    person count bash

---
+++ pc.sh
@@ -0,0 +1,12 @@
+#example:
+#sh pc.sh 1.264 result.mpg
+echo $1
+rm -r 264_imgs
+mkdir 264_imgs
+ffmpeg -i $1 -r 1/1 264_imgs/$filename%04d.jpg
+echo $2
+rm -r result_imgs
+mkdir result_imgs
+echo "Testing"
+python test_frcnn.py -p 264_imgs
+ffmpeg -r 25 -i result_imgs/%4d.png $2
