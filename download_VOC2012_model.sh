a8499cf183265830bde385253fc4d1f4c4361635
peter6888 <qq1800@gmail.com>
2017-08-17 10:37:06 -0700

    Update download_VOC2012_model.sh

--- download_VOC2012_model.sh
+++ download_VOC2012_model.sh
@@ -3,5 +3,5 @@ wget --no-check-certificate "https://onedrive.live.com/download?cid=06DDD24D87B0
 mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21483&authkey=ABx_ACRHH0g3WyE" model_frcnn.hdf5
 wget --no-check-certificate "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21487&authkey=ACAQeaiFqEzF0PI"
 mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21487&authkey=ACAQeaiFqEzF0PI" new_out_model_weight.hdf5
-
-
+wget --no-check-certificate "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21488&authkey=AD9kmvTeuAO8VDg"
+mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21488&authkey=AD9kmvTeuAO8VDg" config.pickle
