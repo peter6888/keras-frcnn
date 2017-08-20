828937546d870c770f2f915b101b513ee46965a1
peter6888 <qq1800@gmail.com>
2017-08-19 21:40:25 -0700

    download 2.264

--- download_264.sh
+++ download_264.sh
@@ -1,2 +1,5 @@
-wget --no-check-certificate "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21482&authkey=ALgHdrTsWIC36LY"
-mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21482&authkey=ALgHdrTsWIC36LY" 1.264
+#!/usr/bin/env bash
+#wget --no-check-certificate "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21482&authkey=ALgHdrTsWIC36LY"
+#mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21482&authkey=ALgHdrTsWIC36LY" 1.264
+wget --no-check-certificate "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21489&authkey=APj2E8TI_O5xjWg"
+mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21489&authkey=APj2E8TI_O5xjWg" 2.264
