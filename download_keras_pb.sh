d65acee112b88d1ef1119edebb2287a3de6bb8b2
peter6888 <qq1800@gmail.com>
2017-09-18 21:10:54 -0700

    download frozen model bash

---
+++ download_keras_pb.sh
@@ -0,0 +1,5 @@
+wget "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21491&authkey=AFEemG84Mmguc6o"
+mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21491" cls.pb
+wget "https://onedrive.live.com/download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21490&authkey=ACi12gRfwKL0m5Q"
+mv "download?cid=06DDD24D87B0D23B&resid=6DDD24D87B0D23B%21490" rpn.pb
+
