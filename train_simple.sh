e7d4940da6ac7e8404eb9856482cc03abd3bcd65
Peter Li <qq1800@gmail.com>
2017-11-29 04:01:23 +0000

    test with MPII head data and get a better result

--- train_simple.sh
+++ train_simple.sh
@@ -1,2 +1,1 @@
-python train_frcnn.py -o simple -p data/Simple/elevator.txt --in
-put_weight_path ./model_frcnn.hdf5 --output_weight_path new_out_model_weight.hdf5
+python train_frcnn.py -o simple -p data/Simple/elevator.txt --input_weight_path ./model_frcnn.hdf5 --output_weight_path new_out_model_weight.hdf5
